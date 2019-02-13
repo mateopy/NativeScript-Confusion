4d8be2ce6c8940a865cc2c5400000426b7c8aed0
Mateo Nuñez <mateo@web.com.py>
2019-02-12 22:17:12 -0300

    Buttons

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
+import { FavoriteService } from '../services/favorite.service';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { switchMap } from 'rxjs/operators';
@@ -17,19 +19,42 @@ export class DishdetailComponent implements OnInit {
   dish: Dish;
   comment: Comment;
   errMess: string;
+  avgstars: string;
+  numcomments: number;
+  favorite: boolean = false;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
-    @Inject('baseURL') private baseURL) { }
+    @Inject('baseURL') private baseURL,
+    private favoriteservice: FavoriteService,
+    private fonticon: TNSFontIconService, 
+    ) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => this.dish = dish,
+      this.route.params
+        .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+        .subscribe(dish => { 
+            this.dish = dish;
+            this.favorite = this.favoriteservice.isFavorite(this.dish.id);
+            this.numcomments = this.dish.comments.length;
+  
+            let total = 0;
+            this.dish.comments.forEach(comment => total += comment.rating);
+            this.avgstars = (total/this.numcomments).toFixed(2);
+          },
           errmess => { this.dish = null; this.errMess = <any>errmess; });
-  }
+    }
+  
+    addToFavorites() {
+      if (!this.favorite) {
+        console.log('Adding to Favorites', this.dish.id);
+        this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+        //const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+        //toast.show();
+      }
+    }
 
   goBack(): void {
     this.routerExtensions.back();

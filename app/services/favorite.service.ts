ee62140899a6e85845f8617bc7e562688775c395
Mateo Nuñez <mateo@web.com.py>
2019-03-04 15:26:20 -0300

    Storage with Couchbase

--- app/services/favorite.service.ts
+++ app/services/favorite.service.ts
@@ -3,14 +3,25 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CouchbaseService } from '../services/couchbase.service';
 
 @Injectable()
 export class FavoriteService {
 
     favorites: Array<number>;
+    docId: string = "favorites";
 
-    constructor(private dishservice: DishService) {
-        this.favorites = [];
+    constructor(private dishservice: DishService,
+        private couchbaseService: CouchbaseService) {
+            this.favorites = [];
+
+            let doc = this.couchbaseService.getDocument(this.docId);
+            if( doc == null) {
+              this.couchbaseService.createDocument({"favorites": []}, this.docId);
+            }
+            else {
+              this.favorites = doc.favorites;
+            }
     }
 
     isFavorite(id: number): boolean {
@@ -18,11 +29,13 @@ export class FavoriteService {
     }
 
     addFavorite(id: number): boolean {
-        if(!this.isFavorite(id)) {
-            this.favorites.push(id);
+        if (!this.isFavorite(id)) {
+          this.favorites.push(id);
+          this.couchbaseService.updateDocument(this.docId, {"favorites": this.favorites});
         }
+    
         return true;
-    }
+      }
 
     getFavorites(): Observable<Dish[]> {
         return this.dishservice.getDishes()
@@ -32,11 +45,12 @@ export class FavoriteService {
     deleteFavorite(id: number): Observable<Dish[]> {
         let index = this.favorites.indexOf(id);
         if (index >= 0) {
-            this.favorites.splice(index,1);
-            return this.getFavorites();
+          this.favorites.splice(index,1);
+          this.couchbaseService.updateDocument(this.docId, {"favorites": this.favorites});
+          return this.getFavorites();
         }
         else {
-            return throwError('Deleting non-existant favorite');
+          return throwError('Deleting non-existant favorite');
         }
-    }
+      }
 }

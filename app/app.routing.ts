f89b2f1c796ccd14b67bc129d81f86b8bc5a9bc3
Mateo Nuñez <mateo@web.com.py>
2019-03-15 00:52:18 -0300

    Camera

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -9,6 +9,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { UserAuthComponent } from "./userauth/userauth.component"; 
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -18,7 +19,8 @@ const routes: Routes = [
     { path: "aboutus", component: AboutComponent },
     { path: "favorites", component: FavoritesComponent },
     { path: "reservation", component: ReservationComponent },
-    { path: 'dishdetail/:id', component: DishdetailComponent }
+    { path: 'dishdetail/:id', component: DishdetailComponent },
+    { path: "auth", component: UserAuthComponent }, 
 ];
 
 @NgModule({
